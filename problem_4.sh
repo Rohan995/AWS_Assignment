3c2e7c9b9819a63f1e2a4a6a7081518804761895
Rohan995 <39547084+Rohan995@users.noreply.github.com>
2018-06-19 16:06:56 +0530

    Update problem_4.sh

--- problem_4.sh
+++ problem_4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-instance1_id=$(aws ec2 run-instances --image-id ami-14c5486b --count 1 --instance-type t2.micro --key-name Nawaz@1995 --security-group-ids sg-66323b2e --subnet-id subnet-de0385f2 --iam-instance-profile Name=PE_trainee_Admin_role --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Rohan},
+instance1_id=$(aws ec2 run-instances --image-id ami-14c5486b --count 1 --instance-type t2.micro --key-name Pe_Rohan --security-group-ids sg-66323b2e --subnet-id subnet-de0385f2 --iam-instance-profile Name=PE_trainee_Admin_role --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Rohan},
 {Key=emailid,Value=rohan.raha@quantiphi.com},{Key=Project,Value=Shell_Assign}]' --user-data '#!/bin/sh
 
 sudo su ec2-user
